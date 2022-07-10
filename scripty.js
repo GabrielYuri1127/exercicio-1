9e167f6758a25447ea6479f5d3e9d26e2e5f9a85
GabrielYuri1127 <gabrielcavalcante1127@gmail.com>
2022-07-10 00:25:24 -0400

    first commit

---
+++ scripty.js
@@ -0,0 +1,10 @@
+var nome = 'gabriel'
+var numero = 17
+var ano_atual = 2022
+console.log(ano_atual-numero)
+console.log(ano_atual+numero)
+console.log("Estou mexendo no console.log")
+let quartaFeira = false
+console.log(quartaFeira)
+console.log(typeof quartaFeira)
+console.log(nome)
